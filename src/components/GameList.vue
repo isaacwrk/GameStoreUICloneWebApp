baafda967ae3942f55db0861582463a7fc22e720
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-12 18:37:01 -0300

    trying to align star rate card

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="whitespace-nowrap overflow-y-auto">
         <div class="flex">
-            <div class="flex  md:flex-row overflow-hidden bg-white">
-                <!-- media -->
-                <div class="h-28 w-28 ml-10 horizon bg-cover bg-no-repeat rounded">
-                </div>
-                <!-- content -->
-                <div class="text-gray-800 flex bg-base-gray w-11 h-20">
-                    <h3 class="font-semibold text-lg leading-tight truncate"></h3>
-                    <p class="mt-2">
-                        dsadsad
-                    </p>
-                    <p class="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
-                        &bull; 
-                    </p>
-                </div>
-            </div><!--/ card-->
+
+            <!-- media -->
+            <div class="h-28 w-28 ml-10 horizon bg-cover bg-no-repeat rounded">
+            </div>
+
+            <!-- content -->
+            <div class="text-font-blue bold flex flex-wrap bg-base-gray w-44 h-24 mt-2">
+                <h3 class="font-semibold text-lg leading-tight truncate"></h3>
+                <p class="mt-2 mr-2 ml-5 text-base font-bold">
+                    Horizon Dawn
+                </p>
+                <p class="text-xs text-gray-500 ml-5 font-semibold mb-10">
+                    Rockstar Games
+                </p>
+                <div class="inline-flex flex-wrap-rev ml-5">
+                    <i class="fa fa-star text-primary-button"></i>
+                    <p class="text-primary-blue text-sm">4.5 (120)</p>
+                </div> 
+            </div>
+
         </div>
     </div>
 </template>
