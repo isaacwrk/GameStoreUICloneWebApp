b41035128c39c646b7eb9a7b93077b31dbc44473
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-15 21:35:52 -0300

    fixed list card

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -1,34 +1,53 @@
 <template>
-    <div class="whitespace-nowrap overflow-y-auto">
-        <div class="flex">
+    <div class="flex items-center">
+        <!-- media -->
+        <div class="h-28 w-28 ml-10 horizon bg-cover bg-no-repeat rounded">
+        </div>
 
-            <!-- media -->
-            <div class="h-28 w-28 ml-10 horizon bg-cover bg-no-repeat rounded">
+        <!-- content -->
+        <div class="text-font-blue bold  bg-base-gray w-44 h-24 flex-1 flex flex-col">
+            <p class="mt-2 mr-2 ml-5 text-base font-bold">
+                Horizon: New Dawn
+            </p>
+            <p class="text-xs text-gray-500 ml-5 font-semibold mb-4">
+                Rockstar Games
+            </p>
+            <div class="flex items-center mb-2 ml-5">
+                <i class="fa fa-star text-primary-button"></i>
+                <p class="text-primary-blue text-sm">4.5 (120)</p>
+                <p class="text-primary-button ml-12 font-bold text-sm">$20.00</p>
             </div>
-
-            <!-- content -->
-            <div class="text-font-blue bold flex flex-wrap bg-base-gray w-44 h-24 mt-2">
-                <h3 class="font-semibold text-lg leading-tight truncate"></h3>
-                <p class="mt-2 mr-2 ml-5 text-base font-bold">
-                    Horizon Dawn
-                </p>
-                <p class="text-xs text-gray-500 ml-5 font-semibold mb-10">
-                    Rockstar Games
-                </p>
-                <div class="inline-flex flex-wrap-rev ml-5">
-                    <i class="fa fa-star text-primary-button"></i>
-                    <p class="text-primary-blue text-sm">4.5 (120)</p>
-                </div> 
-            </div>
-
         </div>
     </div>
+   
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 
 const GameList = defineComponent({
+    props:{
+        titleGame:{
+            type:String,
+            required: true,
+        },
+        gameOwn:{
+            type:String,
+            required:true
+        },
+        starRate:{
+            type:String,
+            required:true
+        },
+        price:{
+            type:String,
+            required:true
+        },
+        poster:{
+            type:String,
+            required:true
+        }
+    }
 });
 
 export default GameList;
@@ -37,5 +56,17 @@ export default GameList;
 <style>
 .horizon{
     background: url('~@/assets/img/horizondawnposter.jpg');
+}
+.dishonored{
+    background: url('~@/assets/img/dishonored2poster.jpg');
+}
+.forza{
+    background: url('~@/assets/img/forza4poster.jpg');
+}
+.assasins{
+    background: url('~@/assets/img/acoriginposter.jpg');
+}
+.reddead{
+    background: url('~@/assets/img/reddeadposter.jpg');
 }
 </style>
