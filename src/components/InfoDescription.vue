9c5afc31b679923d31683df539e0ad6fc37670af
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-17 19:24:05 -0300

    creating title and subtitle in infoscreen

---
+++ src/components/InfoDescription.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="relative -mt-9 bg-white rounded-t-2xl pt-8">
+        <div class="relative ml-10">
+            <p class="text-font-blue text-xl font-bold">Forza 4</p>
+            <p class="text-gray-400 font-bold">Playground Games</p>
+            <i class="absolute bg-primary-button fas fa-heart rounded-full w-10 h-10 text-center p-3 left-64 bottom-1 text-white hover:bg-primary-blue"></i>
+        </div>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const InfoDescription = defineComponent({
+});
+
+export default InfoDescription;
+</script>
+
+<style>
+
+</style>
--- src/views/InfoScreen.vue
+++ src/views/InfoScreen.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <InfoWallpaper icon="fas fa-angle-left" wallpaper="bg-forza"/>
+        <InfoDescription/>
     </div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 import InfoWallpaper from '@/components/InfoWallpaper.vue';
+import InfoDescription from '@/components/InfoDescription.vue';
 
 const InfoScreen = defineComponent({
     components:{
-        InfoWallpaper
+        InfoWallpaper,
+        InfoDescription
     }
 });
 
