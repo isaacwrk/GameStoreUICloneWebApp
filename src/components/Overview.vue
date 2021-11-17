4da45e5b5cc73fee1d6270762d04f17398a1f131
isaacwrk <isaacsouzawrk@gmail.com>
2021-11-17 19:46:32 -0300

    creating overview component

---
+++ src/components/Overview.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p class="ml-10 mt-9 text-font-blue font-bold">Overview</p>
+        <p class="ml-10 mt-2 text-gray-500 text-sm">{{text1}}</p>
+        <p class="ml-10 mt-4 text-gray-500 text-sm">{{text2}}</p>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const Overview = defineComponent({
+    props:{
+        text1:{
+            type:String,
+            required:true
+        },
+        text2:{
+            type:String,
+            required:true
+        }
+    }
+});
+
+export default Overview;
+</script>
+
+<style>
+
+</style>
--- src/views/InfoScreen.vue
+++ src/views/InfoScreen.vue
@@ -2,6 +2,10 @@
     <div>
         <InfoWallpaper icon="fas fa-angle-left" wallpaper="bg-forza"/>
         <InfoDescription/>
+        <Overview
+            text1="Forza Horizon 4 - abbreviated as FH4 - is an open-world racing game developed by Playground Games and Turn 10 Studios, the Steam release was developed by Sumo Digital. The game is set in Great Britain."
+            text2="The title is set in a 'heavily-stylised version of Britain within' Scotland and the Lake District of North West England. It features Edinburgh and villages in Cotswolds as one of many locations."
+        />
     </div>
 </template>
 
@@ -9,11 +13,13 @@
 import { defineComponent } from 'vue';
 import InfoWallpaper from '@/components/InfoWallpaper.vue';
 import InfoDescription from '@/components/InfoDescription.vue';
+import Overview from '@/components/Overview.vue';
 
 const InfoScreen = defineComponent({
     components:{
         InfoWallpaper,
-        InfoDescription
+        InfoDescription,
+        Overview
     }
 });
 
